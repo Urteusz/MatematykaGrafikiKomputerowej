896194a0dffcfd4ab64dbd75375d901b7b4336b0
Urteusz <161340944+Urteusz@users.noreply.github.com>
2025-11-04 21:30:42 +0100

    Zadanie1

---
+++ Vector.h
@@ -0,0 +1,33 @@
+//
+// Created by Urteusz on 22.10.2025.
+//
+
+#ifndef OPENGLEXAMPLE_VECTOR_H
+#define OPENGLEXAMPLE_VECTOR_H
+
+#include <string>
+#include <iostream>
+#include <cmath>
+
+class Vector {
+public:
+    float x,y,z;
+
+    Vector();
+    Vector(float x, float y, float z);
+    Vector(const Vector &v);
+    ~Vector();
+
+    void operator+=(const Vector& v);
+    void add(Vector v);
+    void sub(Vector v);
+    void div(float f);
+    void mag(float f);
+    float length();
+    void normalize();
+    float dotProduct(Vector v);
+    Vector cross(Vector v);
+};
+
+
+#endif //OPENGLEXAMPLE_VECTOR_H
